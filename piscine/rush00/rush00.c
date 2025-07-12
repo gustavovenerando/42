93b7d7a264c78e941ae3933703e3177a6db3c053
gustavovenerando <gvhenrique@hotmail.com>
2025-07-12 15:56:06 -0300

    rush00

---
+++ piscine/rush00/rush00.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gveneran <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/31 11:38:35 by gveneran          #+#    #+#             */
+/*   Updated: 2025/05/31 11:57:44 by gveneran         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_print_row(int width, char begin, char middle, char end)
+{
+	int	column;
+
+	column = 1;
+	while (column <= width)
+	{
+		if (column == 1)
+		{
+			ft_putchar(begin);
+		}
+		else if (column == width)
+		{
+			ft_putchar(end);
+		}
+		else
+		{
+			ft_putchar(middle);
+		}
+		column++;
+	}
+}
+
+void	rush(int x, int y)
+{
+	int	row;
+
+	row = 1;
+	while (row <= y)
+	{
+		if (row == 1)
+		{
+			ft_print_row(x, 'o', '-', 'o');
+		}
+		else if (row == y)
+		{
+			ft_print_row(x, 'o', '-', 'o');
+		}
+		else
+		{
+			ft_print_row(x, '|', ' ', '|');
+		}
+		ft_putchar('\n');
+		row++;
+	}
+}
